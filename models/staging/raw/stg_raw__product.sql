e8688e7d686fc6c5f08b0b49922361ec8137d7cb
Jai Sehgal <jaisehgal@gmail.com>
2024-08-07 09:31:32 +0000

    intermediate tables created

--- models/staging/raw/stg_raw__product.sql
+++ models/staging/raw/stg_raw__product.sql
@@ -10,7 +10,7 @@ renamed as (
 
     select
         products_id,
-        purchse_price AS purchse_price
+        CAST(purchse_price as float64) AS purchase_price
 
     from source
 
--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -10,6 +10,7 @@ renamed as (
 
     select
         date_date,
+        CONCAT (orders_id," - ",pdt_id) as order_product,
         orders_id,
         pdt_id AS products_id,
         revenue,
