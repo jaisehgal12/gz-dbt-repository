fe413c00680854f84cc0ceb8267b1b75a3d30842
Jai Sehgal <jaisehgal@gmail.com>
2024-08-07 09:45:42 +0000

    mart model

---
+++ models/mart/finance_days.sql
@@ -0,0 +1,9 @@
+select
+    op.date_date,
+    count(op.orders_id) as nb_transactions,
+    round(sum(op.revenue), 2) as revenue,
+    round(sum(op.revenue) / count(op.orders_id), 2) as average_basket,
+    round(sum(op.margin), 2) as margin,
+    sum(op.operational_margin) as operational_margin
+from {{ ref("int_orders_operational") }} as op
+group by op.date_date
